58b08f6644d8ac3b989b87b72c77373ffeccd842
liva2 <63157714+liva2@users.noreply.github.com>
2022-10-20 09:32:11 +0700

    Update 2_218.py

--- 2_218.py
+++ 2_218.py
@@ -17,3 +17,13 @@ for x in range(2):
             for w in range(2):
                 if (((x <= z) <= y) or (not w)) == 0:
                     print(x, y, z, w) 
+
+#231 (¬x → y) ∧ (¬y ≡ z) ∧ w
+print('ЕГЭ_2 задача 231')
+print('x,y,z,w')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                if (((not x) <= y) and ((not y) == z) and w) == 1:
+                    print(x, y, z, w)
