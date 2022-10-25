1e4c28fd7563a9539cde57b11ba35c504e2bc069
Людмила <liva2@mail.ru>
2022-10-25 22:27:12 +0700

    add 26- 90

--- 26.py
+++ 26.py
@@ -1,3 +1,26 @@
+#задача 90
+a = open('26-90.txt')
+n = int(next(a).rstrip())
+x = []
+for i in range(n):
+    b = int(next(a).rstrip())
+    x.append(b)
+d = sorted(x,reverse = True)
+s_my = 0
+s_mag = 0
+for i in range(0, n):
+    if i < n // 4:
+        s_my = s_my + d[i] * 0.5
+    else:
+        s_my = s_my + d[i]
+print(s_my )
+d = sorted(x,reverse = False)
+for i in range(0, n):
+    if i < n//4:
+        s_mag  = s_mag + d[i] * 0.5
+    else:
+        s_mag = s_mag + d[i]
+print(s_mag )
 #задача 95
 # a = open('26-95.txt')
 # n = int(next(a).rstrip())
@@ -31,25 +54,25 @@
 #*****************************************************************************************************
 
 # задача 96
-
-a = open('26-96.txt')                       #встаём на начало файла
-n = int(next(a).rstrip())                   #считываем 1-ю строчку файла
-#print(n)
-x = {}                                      #словарь ключ - долгота, значение - широта
-for i in range(n):
-    b, c = map(int,next(a).rstrip().split())    # читаем широту долготу
-    if b >= 0:
-        modB =b // 10
-    else:                                       # если широта < 0, то инвертируем в >0 затем добавим знак -
-        modB = -(-b // 10)
-    x[c] = x.get(c,[]) + [modB]                 # значение в словаре это список целых частей широт делённых на 10
-#print(x)
-m = 1
-for i, j in x.items():                          # проходим по ключ -значениям словаря
-    if len(j)> m:                               # выбираем спиок значений наибольшей длины
-        m = len(j)
-        keyy = i                                # запоминаем ключ и сам список
-        dolg = j
-print(dolg)
-print(keyy, len(set(dolg)))                     # печатаем долготу и количество сигналов
-
+#
+# a = open('26-96.txt')                       #встаём на начало файла
+# n = int(next(a).rstrip())                   #считываем 1-ю строчку файла
+# #print(n)
+# x = {}                                      #словарь ключ - долгота, значение - широта
+# for i in range(n):
+#     b, c = map(int,next(a).rstrip().split())    # читаем широту долготу
+#     if b >= 0:
+#         modB =b // 10
+#     else:                                       # если широта < 0, то инвертируем в >0 затем добавим знак -
+#         modB = -(-b // 10)
+#     x[c] = x.get(c,[]) + [modB]                 # значение в словаре это список целых частей широт делённых на 10
+# #print(x)
+# m = 1
+# for i, j in x.items():                          # проходим по ключ -значениям словаря
+#     if len(j)> m:                               # выбираем спиок значений наибольшей длины
+#         m = len(j)
+#         keyy = i                                # запоминаем ключ и сам список
+#         dolg = j
+# print(dolg)
+# print(keyy, len(set(dolg)))                     # печатаем долготу и количество сигналов
+#
